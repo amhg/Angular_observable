6ee859532840e38635381f93774056d605365c85
anhern <andrea.hernandez@daimler.com>
2021-08-14 16:18:48 +0200

    Add project

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  activateUser: boolean = false;
+  private activatedSub: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
+      this.activateUser = didActivate;
+    })
+  }
+
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe();
+  }
+
+}
